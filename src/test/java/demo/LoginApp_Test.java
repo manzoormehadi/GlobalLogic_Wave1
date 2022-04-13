3486855fa96dbf634fca82ddb34b12e6c5d6e5f8
manzoor <manzoor.mehadi@niit.com>
2022-04-13 15:47:54 +0530

    code

---
+++ src/test/java/demo/LoginApp_Test.java
@@ -0,0 +1,38 @@
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginApp_Test {
+	WebDriver driver;
+	@Test
+	public void testLogin() {
+		driver.findElement(By.linkText("Log in")).click();
+		driver.findElement(By.id("Email")).sendKeys("manz@mass.com");
+		driver.findElement(By.id("Password")).sendKeys("mass123");
+		driver.findElement(By.xpath("//input[@value='Log in']")).click();
+		boolean loginLink=driver.findElement(By.linkText("manz@mass.com")).isDisplayed();
+		Assert.assertEquals(true, loginLink);
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		WebDriverManager.chromedriver().setup();
+
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://demowebshop.tricentis.com/login");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.close();
+	}
+}
